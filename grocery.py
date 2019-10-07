0311d235d9b861f8cfcb6580085d72250a8263a0
mohammedshahid2406 <55689725+mohammedshahid2406@users.noreply.github.com>
2019-10-07 08:41:40 +0530

    Add files via upload

---
+++ grocery.py
@@ -0,0 +1,16 @@
+x=str(input("enter the name of the item :"))
+y=int(input("enter the quantity :"))
+z=int(input("enter the price per unit :"))
+
+
+print("**************BILL************\n")
+
+
+print("Item Name "  "Item Quantity "   "item price" )
+print("******************************\n")
+
+total_amt=y*z
+print("Total Amount to be paid ", total_amt)
+
+
+print("****************************")
