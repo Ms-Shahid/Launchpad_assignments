6fd7eb67bee04e73846b697ffaae1ae7be24aa81
mohammedshahid2406 <55689725+mohammedshahid2406@users.noreply.github.com>
2019-10-07 08:40:59 +0530

    Add files via upload

---
+++ cir.py
@@ -0,0 +1,4 @@
+radius= float(input("enter the radius of the circle:"))
+area = 3.14*radius*radius
+circumferance= 2*3.14*radius
+print("area = "+str(round(area,2)) +"\t circumferance ="+str(round(circumferance,2)))
